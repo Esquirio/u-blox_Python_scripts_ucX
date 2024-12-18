1e666ad83bfb992c8312c63ce7b4b550795df797
Fernando Esquirio Torres <31143274+Esquirio@users.noreply.github.com>
2024-12-18 10:15:57 +0100

    fix: fixing the client.py to run all files at once

--- http_server_client/my-http-server/src/client.py
+++ http_server_client/my-http-server/src/client.py
@@ -3,7 +3,8 @@ import time
 from datetime import datetime
 
 images_list = ["image_33kb.jpg", "image_53kb.jpg", "image_100kb.jpg", "image_500kb.jpg",
-               "image_1mb.jpg", "image_5mb.jpg", "image_10mb.jpg", "image_20mb.jpg"]
+               "image_1mb.jpg", "image_5mb.jpg", "image_10mb.jpg", "image_20mb.jpg",
+               "image_105mb.jpg"]
 
 def fetch_image(image_url, save_path, image_name, result_file):
     start_time = time.time()
@@ -13,7 +14,7 @@ def fetch_image(image_url, save_path, image_name, result_file):
     if response.status_code == 200:
         with open(save_path, 'wb') as file:
             file.write(response.content)
-        # print(f'Image saved to {save_path}') # for debug
+        # print(f'Image saved to {save_path}')
         
         # Calculate transfer time and throughput
         transfer_time = (end_time - start_time) * 1000  # in milliseconds
@@ -33,7 +34,7 @@ def fetch_image(image_url, save_path, image_name, result_file):
             f'***************************************************\n\n'
         )
         
-        # print(result) # For debug
+        # print(result)
         
         with open(result_file, 'a') as file:
             file.write(result)
@@ -44,11 +45,11 @@ if __name__ == "__main__":
     N = 100  # Number of times to run the download process
     ip_address = "192.168.2.100"
     port = 4043
-    filename = datetime.now().strftime('%Y.%m.%d.%H.%M.%S_results.txt')
     
     for _ in range(N):
-        image_name = images_list[8]
-        image_url = f'http://{ip_address}:{port}/{image_name}'  # URL of the image on the server
-        save_path = "sample_received.jpg"
-        # save_path = image_name  # Path to save the image
-        fetch_image(image_url, save_path, image_name, filename)
+        filename = datetime.now().strftime('%Y.%m.%d.%H.%M.%S_results.txt')
+        for image_name in images_list:
+            image_url = f'http://{ip_address}:{port}/{image_name}'  # URL of the image on the server
+            # save_path = "sample_received.jpg"
+            save_path = image_name  # Path to save the image
+            fetch_image(image_url, save_path, image_name, filename)
