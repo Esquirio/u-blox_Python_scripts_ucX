524a4b10bafbbfc55713a6bc582ea59e9ae44946
Fernando Esquirio Torres <esquirio@gmail.com>
2025-01-09 19:18:28 +0100

    feat: update to use u-blox SPS and send data similar to Bluetooth Classic.

--- bluetooth/ubx_send_data_bt.py
+++ bluetooth/ubx_send_data_bt.py
@@ -3,6 +3,7 @@ import argparse
 import random
 import string
 import asyncio
+import math
 from colorama import init, Fore, Style
 
 init(autoreset=True)
@@ -40,18 +41,17 @@ def send_data_via_bluetooth_classic(sock, data):
     try:
         # Send data
         sock.send(data)
-        hex_data = data_to_hex(data)
-        print(Fore.GREEN + f"Sent data: {hex_data}")
+        print(Fore.GREEN + f"Sending {len(data)} bytes:\n{data}")
     except bluetooth.BluetoothError as e:
         print(Fore.RED + f"Bluetooth error: {e}")
 
-async def find_service_and_characteristic(client, service_prefix, characteristic_prefix):
+async def find_service_and_characteristic(client, service_uuid, characteristic_uuid):
     """
-    Finds and returns the first service and characteristic UUIDs that match the given prefixes.
+    Finds and returns the service and characteristic UUIDs that match the given UUIDs.
 
     :param client: The BleakClient instance.
-    :param service_prefix: The prefix of the service UUID to match.
-    :param characteristic_prefix: The prefix of the characteristic UUID to match.
+    :param service_uuid: The service UUID to match.
+    :param characteristic_uuid: The characteristic UUID to match.
     :return: The matched service and characteristic UUIDs, or None if not found.
     """
     if client.is_connected:
@@ -60,69 +60,124 @@ async def find_service_and_characteristic(client, service_prefix, characteristic
         # Get all services
         services = client.services
         for service in services:
-            if service.uuid.startswith(service_prefix):
+            if service.uuid == service_uuid:
                 print(Fore.CYAN + f"Matching service found: {service.uuid}")
                 for characteristic in service.characteristics:
-                    if characteristic.uuid.startswith(characteristic_prefix):
+                    if characteristic.uuid == characteristic_uuid:
                         print(Fore.CYAN + f"Matching characteristic found: {characteristic.uuid}")
                         return service.uuid, characteristic.uuid
 
         print(Fore.RED + "No matching service or characteristic found.")
         return None, None
 
-async def write_gatt_char_ble(client, data, service_prefix, characteristic_prefix):
+async def send_data_sps(client, data, characteristic_uuid):
+    """
+    Sends data via Bluetooth Low Energy (BLE) using SPS.
+
+    :param client: The BleakClient instance.
+    :param data: The data to send.
+    :param characteristic_uuid: The characteristic UUID to match.
+    """
+    
+    # Limit the data length to 4148 bytes
+    # (the maximum data length was found by testing with the u-blox NINA-B2 module)
+    if len(data) > 4148:
+        data = data[:4148]
+        print(Fore.YELLOW + "Data length exceeds 4148 bytes. Limiting to 4148 bytes.")
+    
+    await client.start_notify(characteristic_uuid, notification_handler)
+    await client.write_gatt_char(characteristic_uuid, data.encode())
+    
+    print(Fore.GREEN + f"Sending {len(data)} bytes:\n{data}")
+
+    await asyncio.sleep(1)  # Wait for notification
+    await client.stop_notify(characteristic_uuid)
+
+async def write_data_gatt(client, data, characteristic_uuid):
+    """
+    Sends data via Bluetooth Low Energy (BLE) for customized services and characteristics.
+
+    :param client: The BleakClient instance.
+    :param data: The data to send.
+    :param characteristic_uuid: The characteristic UUID to match.
+    """
+    
+    # Limit the data length to 244 bytes as per u-blox documentation
+    # Document: u-connectXpress-ATCommands-Manual_UBX-14044127
+    # Section: 12.2 GATT Define a characteristic +UBTGCHA
+    if len(data) > 244:
+        data = data[:244]
+        print(Fore.YELLOW + "Data length exceeds 244 bytes. Limiting to 244 bytes.")
+    
+    await client.write_gatt_char(characteristic_uuid, data.encode())
+    hex_data = data_to_hex(data)
+    print(Fore.GREEN + f"Sending {len(data)} bytes:\n{hex_data}")
+
+def notification_handler(sender, data):
+    """
+    Handles notifications from the BLE device.
+
+    :param sender: The sender of the notification.
+    :param data: The data received in the notification.
+    """
+    hex_data = ' '.join(f"{byte:02X}" for byte in data)
+    print(Fore.YELLOW + f"Notification from {sender}: {hex_data}")
+
+async def write_gatt_char_ble(client, data, service_uuid, characteristic_uuid):
     """
     Sends data via Bluetooth Low Energy (BLE).
 
     :param client: The BleakClient instance.
     :param data: The data to send.
-    :param service_prefix: The prefix of the service UUID to match.
-    :param characteristic_prefix: The prefix of the characteristic UUID to match.
+    :param service_uuid: The service UUID to match.
+    :param characteristic_uuid: The characteristic UUID to match.
     """
-    service_uuid, characteristic_uuid = await find_service_and_characteristic(client, service_prefix, characteristic_prefix)
+    service_uuid, characteristic_uuid = await find_service_and_characteristic(client, service_uuid, characteristic_uuid)
     
     if service_uuid and characteristic_uuid:
-        # Limit the data length to 244 bytes as per u-blox documentation
-        # Document: u-connectXpress_UserGuide_UBX-16024251
-        # Section: 12.2 GATT Define a characteristic +UBTGCHA
-        if len(data) > 244:
-            data = data[:244]
-            print(Fore.YELLOW + "Data length exceeds 244 bytes. See ublox documentation.")
-        
-        await client.write_gatt_char(characteristic_uuid, data.encode())
-        hex_data = data_to_hex(data)
-        print(Fore.GREEN + f"Sent data: {hex_data}")
+        # Check if the service and characteristic UUIDs start with "2456e1b9"
+        if service_uuid.startswith("2456e1b9") and characteristic_uuid.startswith("2456e1b9"):
+            await send_data_sps(client, data, characteristic_uuid)
+        else:
+            await write_data_gatt(client, data, characteristic_uuid)
     else:
         print(Fore.RED + "Service or characteristic not found. Cannot send data.")
 
-async def read_gatt_char_value(client, service_prefix, characteristic_prefix):
+async def read_gatt_char_value(client, service_uuid, characteristic_uuid, sps=False):
     """
     Finds the matching service and characteristic and reads data from the Bluetooth device.
 
     :param client: The BleakClient instance.
-    :param service_prefix: The prefix of the service UUID to match.
-    :param characteristic_prefix: The prefix of the characteristic UUID to match.
+    :param service_uuid: The service UUID to match.
+    :param characteristic_uuid: The characteristic UUID to match.
     """
-    service_uuid, characteristic_uuid = await find_service_and_characteristic(client, service_prefix, characteristic_prefix)
+    service_uuid, characteristic_uuid = await find_service_and_characteristic(client, service_uuid, characteristic_uuid)
 
     if service_uuid and characteristic_uuid:
         if client.is_connected:
             print(Fore.CYAN + f"Reading data from characteristic {characteristic_uuid}...")
             data = await client.read_gatt_char(characteristic_uuid)
 
-            # Convert the bytes data to a hex string and print it
-            hex_data = ' '.join(f"{byte:02X}" for byte in data)
-            print(Fore.GREEN + f"Data read: {hex_data}")
+            if sps:
+                # Convert the bytes data to a string and print it
+                data = data.decode()
+                print(Fore.GREEN + f"Data read: {data}")
+            else:
+                # Convert the bytes data to a hex string and print it
+                hex_data = ' '.join(f"{byte:02X}" for byte in data)
+                print(Fore.GREEN + f"Data read: {hex_data}")
     else:
         print(Fore.RED + "Service or characteristic not found. Cannot read data.")
 
-async def main_ble(target_address, service_prefix="4906276b", characteristic_prefix="49af5250"):
+async def main_ble(target_address, service_uuid, characteristic_uuid, max_data_size, xtimes):
     """
     Sends data via Bluetooth Low Energy (BLE) and reads the characteristic value after writing.
 
     :param target_address: The target Bluetooth address.
-    :param service_prefix: The prefix of the service UUID to match.
-    :param characteristic_prefix: The prefix of the characteristic UUID to match.
+    :param service_uuid: The service UUID to match.
+    :param characteristic_uuid: The characteristic UUID to match.
+    :param max_data_size: The maximum number of data to generate.
+    :param xtimes: The number of times the script should run.
     """
     try:
         if BleakClient is None:
@@ -131,26 +186,31 @@ async def main_ble(target_address, service_prefix="4906276b", characteristic_pre
         async with BleakClient(target_address) as client:
             print(Fore.CYAN + f"Connected to {target_address} via BLE")
 
-            while True:
-                length = int(input("Enter the number of characters for the data: ").strip())
-                data = generate_random_data(length)
-
-                await write_gatt_char_ble(client, data, service_prefix, characteristic_prefix)
-                await read_gatt_char_value(client, service_prefix, characteristic_prefix)
-                
-                more = input("Do you want to send more data? (Y/N): ").strip().lower()
-                if more != 'y' and more != '':
-                    break
+            if xtimes == 0:
+                data = generate_random_data(max_data_size)
+                await write_gatt_char_ble(client, data, service_uuid, characteristic_uuid)
+                await read_gatt_char_value(client, service_uuid, characteristic_uuid, True)
+            else:
+                for i in range(1, max_data_size + 1):
+                    for _ in range(xtimes):
+                        data = generate_random_data(i)
+                        await write_gatt_char_ble(client, data, service_uuid, characteristic_uuid)
+                        await read_gatt_char_value(client, service_uuid, characteristic_uuid)
+                        await asyncio.sleep(1)
     except Exception as e:
         print(Fore.RED + f"BLE error: {e}")
 
-def main_classic(target_address, port):
+async def main_classic(target_address, port, max_data_size, xtimes):
     """
     Sends data via Bluetooth Classic.
 
     :param target_address: The target Bluetooth address.
     :param port: The port to connect to.
+    :param max_data_size: The maximum number of data to generate.
+    :param xtimes: The number of times the script should run.
     """
+
+    div_data = 2500
     # Create a Bluetooth socket
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     
@@ -159,14 +219,16 @@ def main_classic(target_address, port):
         sock.connect((target_address, port))
         print(Fore.CYAN + f"Connected to {target_address} on port {port}")
         
-        while True:
-            length = int(input("Enter the number of characters for the data: ").strip())
-            data = generate_random_data(length)
+        if xtimes == 0:
+            data = generate_random_data(max_data_size)
             send_data_via_bluetooth_classic(sock, data)
-            
-            more = input("Do you want to send more data? (Y/N): ").strip().lower()
-            if more != 'y' and more != '':
-                break
+            await asyncio.sleep(math.ceil(len(data) / div_data) + 1) # Wait for data to be read by u-blox module
+        else:
+            for i in range(1, max_data_size + 1):
+                for _ in range(xtimes):
+                    data = generate_random_data(i)
+                    send_data_via_bluetooth_classic(sock, data)
+                    await asyncio.sleep(math.ceil(len(data) / div_data) + 1) # Wait for data to be read by u-blox module
     except bluetooth.BluetoothError as e:
         print(Fore.RED + f"Bluetooth error: {e}")
     finally:
@@ -189,15 +251,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Send data via Bluetooth")
     parser.add_argument("-a", "--address", required=True, help="Target Bluetooth address")
     parser.add_argument("-t", "--type", choices=["classic", "ble"], required=True, help="Type of Bluetooth connection (classic or ble)")
+    parser.add_argument("--data_size", type=int, required=True, help="Maximum number of characters to send")
+    parser.add_argument("--xtimes", type=int, required=True, help="Number of times the script should run for each data size")
     args = parser.parse_args()
     
-    service_prefix = "4906276b"
-    characteristic_prefix = "49af5250"
+    service_uuid = "2456e1b9-26e2-8f83-e744-f34f01e9d701"
+    characteristic_uuid = "2456e1b9-26e2-8f83-e744-f34f01e9d703"
 
     target_address = format_mac_address(args.address)
     port = 1  # Commonly used port for RFCOMM
     
     if args.type == "classic":
-        main_classic(target_address, port)
+        asyncio.run(main_classic(target_address, port, args.data_size, args.xtimes))
     elif args.type == "ble":
-        asyncio.run(main_ble(target_address, service_prefix, characteristic_prefix))
+        asyncio.run(main_ble(target_address, service_uuid, characteristic_uuid, args.data_size, args.xtimes))
